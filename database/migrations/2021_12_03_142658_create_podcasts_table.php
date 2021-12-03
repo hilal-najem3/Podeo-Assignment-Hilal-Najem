c97f1e670e9a2f3e4a4d68aa0c4b7681898786b5
hilal-najem <hilal123work@gmail.com>
2021-12-03 16:41:24 +0200

    Add Podcast model and migration

---
+++ database/migrations/2021_12_03_142658_create_podcasts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePodcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->string('link');
+            $table->unsignedBigInteger('admin_id')->nullable();
+            $table->timestamps();
+            
+            $table->foreign('admin_id')->references('id')->on('admins')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('podcasts', function (Blueprint $table) {
+            $table->dropForeign(['admin_id']);
+        });
+        Schema::dropIfExists('podcasts');
+    }
+}
