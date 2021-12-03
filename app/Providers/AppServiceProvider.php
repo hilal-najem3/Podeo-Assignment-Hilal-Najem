043c40b7f82f685298858771c9007c824b945c1d
hilal-najem <hilal123work@gmail.com>
2021-12-03 16:15:53 +0200

    First Commit
    Laravel web application that takes care of loggin in users and admins using guards with their specific routes and basic needed implementations

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use App;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $environment = App::environment();
+        if ($environment === 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+    }
+}
